2622074ebc27ca57763dced3555d897e3a0842aa
lucyrthomas <114244734+lucyrthomas@users.noreply.github.com>
2023-04-20 19:40:12 -0400

    Update vowel clusters.R

--- Vowel Cluster Identifier/vowel clusters.R
+++ Vowel Cluster Identifier/vowel clusters.R
@@ -1,7 +1,7 @@
 library(stringr)
 
 count_vowel_clusters <- function(word){
-  vowel_clusters <- str_extract_all(word, "[AEIOUYaeiouy][AEIOUYaeiouy]+")
+  vowel_clusters <- str_extract_all(word, "[AEIOUYaeiouy][AEIOUYaeiouy]+") #this line identifies two or more vowels in a row
   num_vowel_clusters <- length(vowel_clusters)
   paste0("The word '", word, "' has ", num_vowel_clusters, " vowel clusters: ", vowel_clusters)
 }
